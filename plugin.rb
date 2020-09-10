39863a1616110cfd5b397d41c83a87ae0a7bc59c
oliver-walker-hms <olwa@hms.se>
2020-09-10 13:46:57 -0400

    rev version number

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: private-topics
 # about: Searchable private category topics
-# version: 0.3
+# version: 0.4
 # authors: HMS Networks Americas Solution Center
 # url: https://github.com/hms-networks/discourse-private-topic
 
